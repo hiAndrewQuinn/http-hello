c05b3803ceb9a4a41d28f780b6ed6f66b3affb90
Andrew Quinn <53230903+hiAndrewQuinn@users.noreply.github.com>
2023-03-19 08:19:06 +0200

    initial commit. small hello world-esque http server

--- http-hello/Program.cs
+++ http-hello/Program.cs
@@ -1,3 +1,86 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace http_hello
+{
+    class HttpServerjjj
+    {
+        public static HttpListener listener;
+        public static string url = "http://localhost:8080/";
+        public static int pageViews = 0;
+        public static int requestCount = 0;
+
+        public static string pageData =
+            "<!DOCTYPE html>" +
+            "<html>" +
+            "  <head>" +
+            "    <title>Page Views</title>" +
+            "  </head>" +
+            "  <body>" +
+            "    <h1>Page Views</h1>" +
+            "    <p>This page has been viewed {0} times.</p>" +
+            "    <form method=\"post\" action=\"shutdown\">" +
+            "      <input type=\"submit\" value=\"Shutdown\" />" +
+            "    </form>" +
+            "  </body>" +
+            "</html>";
+
+        public static async Task HandleIncomingConnections()
+        {
+            bool runServer = true;
+
+            while (runServer)
+            {
+                HttpListenerContext ctx = await listener.GetContextAsync();
+
+                HttpListenerRequest req = ctx.Request;
+                HttpListenerResponse resp = ctx.Response;
+
+                Console.WriteLine("Request #{0}", ++requestCount);
+                Console.WriteLine(req.Url.ToString());
+                Console.WriteLine(req.HttpMethod);
+                Console.WriteLine(req.UserHostName);
+                Console.WriteLine(req.UserAgent);
+                Console.WriteLine("------------------------");
+
+                if ((req.HttpMethod == "POST") && req.Url.AbsolutePath == "/shutdown")
+                {
+                    Console.WriteLine("Shutdown requested");
+                    runServer = false;
+                }
+
+                if (req.Url.AbsolutePath != "/favicon.ico")
+                {
+                    pageViews += 1;
+                }
+
+                string disableSubmit = !runServer ? "disabled" : "";
+                byte[] data = Encoding.UTF8.GetBytes(String.Format(pageData, pageViews, disableSubmit));
+                resp.ContentType = "text/html";
+                resp.ContentEncoding = Encoding.UTF8;
+                resp.ContentLength64 = data.LongLength;
+
+                await resp.OutputStream.WriteAsync(data, 0, data.Length);
+                resp.Close();
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            listener = new HttpListener();
+            listener.Prefixes.Add(url);
+            listener.Start();
+            Console.WriteLine("Listening for connections on {0}", url);
+
+            Task listenTask = HandleIncomingConnections();
+            listenTask.GetAwaiter().GetResult();
+
+            listener.Close();
+        }
+    }
+}
